8080ca59261f07318ca72c1894064637c82b54e5
John Hanson <jhansoniv@sbcglobal.net>
2013-10-01 18:00:43 -0500

    Added database to allow script to crash and be recovered without spamming the phone. Not sure if the de-dup is working at the moment. Phone is out of commission at the moment.

--- pushReddit.py
+++ pushReddit.py
@@ -11,18 +11,33 @@ import re
 import time
 import pushover
 import syslog
+import MySQLdb
 
 #Declare variables
-application = ##application
-user = ##user
+application = ##application##
+user = ##user##
+
+#Database connection init
+db = MySQLdb.connect("localhost",##username##,##password##,"pushReddit")
+cursor = db.cursor()
+
+#Database search functions
+def insert(ID, name):
+	try:
+		cursor.execute("INSERT INTO Done values (%s,%s)",(ID, name))
+		db.commit()
+	except:
+		db.rollback()
+
+def getExisting():
+	cursor.execute("SELECT id from Done")
+	data = cursor.fetchall()
+	return data
 
 # Setup pushover and the useragent for praw
 pushover.init(application)
 client = pushover.Client(user)
 r = praw.Reddit(user_agent='New post checker by /u/nubzzz1836')
-
-# Init the list of completed post ids
-already_done = []
 
 while True:
 	# get your subreddit's newest 20 posts
@@ -31,6 +46,7 @@ while True:
 	for x in submissions:
 		# Pull out all 'For Sale' posts 
 		match = re.search('WT[TB]', str(x))
+		already_done = getExisting()
 		if x.id not in already_done and not match:
 			# strip and send a push notification for all posts
 			stripped_title = str(re.sub(r'([0-9]\s::\s)', '', str(x), flags=re.IGNORECASE))
@@ -38,5 +54,5 @@ while True:
 			print 'Message sent:  New r/pipetobaccomarket Post - ' + message
 			client.send_message(message, title="New r/pipetobaccomarket Post", priority=1)
 			syslog.syslog('PushReddit - Message sent:  New r/pipetobaccomarket Post - ' + message)
-			already_done.append(x.id)
+			insert(x.id, stripped_title)
 	time.sleep(300) #sleep for 5 minutes and do it all over again
