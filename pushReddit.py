518e9c5935fc2910876bfe61281219666a334e33
John Hanson <nintendolover581@gmail.com>
2013-10-01 21:56:35 -0500

    Added a main function. Old habits die hard

--- pushReddit.py
+++ pushReddit.py
@@ -44,23 +44,27 @@ pushover.init(application)
 client = pushover.Client(user)
 r = praw.Reddit(user_agent='New post checker by /u/nubzzz1836')
 
+# Pushover functions
+
+def main():
+	submissions = r.get_subreddit('pipetobaccomarket').get_new(limit=20)
+	# iterate through them
+	for x in submissions:
+		# Pull out all 'For Sale' posts 
+		match = re.search('WT[TB]', str(x))
+		already_done = getExisting()
+		if x.id not in already_done and not match:
+			# strip and send a push notification for all posts
+			stripped_title = str(re.sub(r'([0-9]\s::\s)', '', str(x), flags=re.IGNORECASE))
+			message = stripped_title + ' - ' + x.short_link
+			print 'Message sent:  New r/pipetobaccomarket Post - ' + message
+			client.send_message(message, title="New r/pipetobaccomarket Post", priority=1)
+			syslog.syslog('PushReddit - Message sent:  New r/pipetobaccomarket Post - ' + message)
+			insert(x.id, stripped_title)
+	time.sleep(300)
+
 try:
 	while True:
-		# get your subreddit's newest 20 posts
-		submissions = r.get_subreddit('pipetobaccomarket').get_new(limit=20)
-		# iterate through them
-		for x in submissions:
-			# Pull out all 'For Sale' posts 
-			match = re.search('WT[TB]', str(x))
-			already_done = getExisting()
-			if x.id not in already_done and not match:
-				# strip and send a push notification for all posts
-				stripped_title = str(re.sub(r'([0-9]\s::\s)', '', str(x), flags=re.IGNORECASE))
-				message = stripped_title + ' - ' + x.short_link
-				print 'Message sent:  New r/pipetobaccomarket Post - ' + message
-				client.send_message(message, title="New r/pipetobaccomarket Post", priority=1)
-				syslog.syslog('PushReddit - Message sent:  New r/pipetobaccomarket Post - ' + message)
-				insert(x.id, stripped_title)
-		time.sleep(300) #sleep for 5 minutes and do it all over again
+		main()
 except:
 	db.close()
