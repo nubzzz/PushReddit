bf04b7889511b27ce409e4de2196a7501c74b73c
nubzzz <nubzzz@dracut.nubzzz.com>
2018-02-21 22:19:03 +0100

    Fixed for new version on praw Fixed mysql bad char issue

--- pushReddit.py
+++ pushReddit.py
@@ -27,6 +27,8 @@ db_pass = data["database"]["password"]
 db_database = data["database"]["database"]
 db_host = data["database"]["host"]
 praw_useragent = data["praw"]["useragent"]
+praw_client_id = data["praw"]["client_id"]
+praw_client_secret = data["praw"]["client_secret"]
 
 # Database connection init
 db = MySQLdb.connect(db_host,db_user,db_pass,db_database)
@@ -57,21 +59,26 @@ def getExisting():
 # Setup pushover and the useragent for praw
 pushover.init(application)
 client = pushover.Client(user)
-r = praw.Reddit(user_agent=praw_useragent)
+r = praw.Reddit(user_agent=praw_useragent,
+                client_id=praw_client_id,
+                client_secret=praw_client_secret)
 
 # Main Function
 def main():
-	submissions = r.get_subreddit('pipetobaccomarket').get_new(limit=20)
+        submissions = r.subreddit('pipetobaccomarket').new(limit=20)
+	#submissions = r.get_subreddit('pipetobaccomarket').get_new(limit=20)
 	# iterate through them
 	for x in submissions:
 		# Discard all Wanting to Trade/Wanting to Buy posts, leaving only Wanting to Sell or untagged posts
-		match = re.search('WT[TB]', str(x))
+		match = re.search('WT[TB]', str(x.title))
 		# Pull the list of ids from the database
 		already_done = getExisting()
 		if x.id not in already_done and not match:
 			# strip title of post and assemble message
-			stripped_title = str(re.sub(r'([0-9]+\s::\s)', '', str(x), flags=re.IGNORECASE))
-			message = stripped_title + ' - ' + x.short_link
+			stripped_title = str(re.sub(r'([0-9]+\s::\s)', '', str(x.title), flags=re.IGNORECASE))
+			short_link = 'http://redd.it/%s' % (x.id)
+			message = stripped_title + ' - ' + short_link
+                        escaped_title = MySQLdb.escape_string(stripped_title)
 			# print out to stdout
 			print 'Message sent:  New r/pipetobaccomarket Post - ' + message
 			# send message via Pushover
@@ -79,7 +86,7 @@ def main():
 			# Log to syslog for documentation sake (will be replaced by logger at some point)
 			syslog.syslog('PushReddit - Message sent:  New r/pipetobaccomarket Post - ' + message)
 			# Add post ID to the database
-			insert(x.id, stripped_title, x.short_link, x.created_utc)
+			insert(x.id, escaped_title, short_link, x.created_utc)
 	# Run every 300 seconds (5 minutes)
 	s.enter(300, 1, main, ())
 
