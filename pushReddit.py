6525911265304166f516a6341b9cb7b488068398
nubzzz <nubzzz@dracut.nubzzz.com>
2018-03-08 21:21:44 +0100

    Moving to python 3

--- pushReddit.py
+++ pushReddit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 """
 PushReddit
@@ -11,9 +11,9 @@ import re
 import time
 import pushover
 import syslog
-import MySQLdb
-import sched
+import pymysql
 import json
+import sys
 
 # Read in config file
 with open('config.json') as data_file:
@@ -31,30 +31,27 @@ praw_client_id = data["praw"]["client_id"]
 praw_client_secret = data["praw"]["client_secret"]
 
 # Database connection init
-db = MySQLdb.connect(db_host,db_user,db_pass,db_database)
+db = pymysql.connect(db_host,db_user,db_pass,db_database)
 cursor = db.cursor()
-
-# Setup the scheduler
-s = sched.scheduler(time.time, time.sleep)
 
 # Database insert function
 def insert(ID, name, url, date):
-	try:
-		cmd = "INSERT INTO Done values (%s,%s,%s,%s)"
-		cursor.execute(cmd,(ID, name, url, date))
-		db.commit()
-	except:
-		db.rollback()
-		
+    try:
+        stripped_name = db.escape(name)
+        cursor.execute("INSERT INTO Done values ({},{},{},{})".format(ID,stripped_name,url,date))
+        db.commit()
+    except:
+        db.rollback()
+        
 # Database id function - returns list of ids
 def getExisting():
-	cursor.execute("SELECT id from Done")
-	data = cursor.fetchall()
-	dataList = []
-	for i in data:
-		for a in i:
-			dataList.append(a)
-	return dataList
+    cursor.execute("SELECT id from Done")
+    data = cursor.fetchall()
+    dataList = []
+    for i in data:
+        for a in i:
+            dataList.append(a)
+    return dataList
 
 # Setup pushover and the useragent for praw
 pushover.init(application)
@@ -65,36 +62,42 @@ r = praw.Reddit(user_agent=praw_useragent,
 
 # Main Function
 def main():
-        submissions = r.subreddit('pipetobaccomarket').new(limit=20)
-	#submissions = r.get_subreddit('pipetobaccomarket').get_new(limit=20)
-	# iterate through them
-	for x in submissions:
-		# Discard all Wanting to Trade/Wanting to Buy posts, leaving only Wanting to Sell or untagged posts
-		match = re.search('WT[TB]', str(x.title))
-		# Pull the list of ids from the database
-		already_done = getExisting()
-		if x.id not in already_done and not match:
-			# strip title of post and assemble message
-			stripped_title = str(re.sub(r'([0-9]+\s::\s)', '', str(x.title), flags=re.IGNORECASE))
-			short_link = 'http://redd.it/%s' % (x.id)
-			message = stripped_title + ' - ' + short_link
-                        escaped_title = MySQLdb.escape_string(stripped_title)
-			# print out to stdout
-			print 'Message sent:  New r/pipetobaccomarket Post - ' + message
-			# send message via Pushover
-			client.send_message(message, title="New r/pipetobaccomarket Post", priority=1)
-			# Log to syslog for documentation sake (will be replaced by logger at some point)
-			syslog.syslog('PushReddit - Message sent:  New r/pipetobaccomarket Post - ' + message)
-			# Add post ID to the database
-			insert(x.id, escaped_title, short_link, x.created_utc)
-	# Run every 300 seconds (5 minutes)
-	s.enter(300, 1, main, ())
+    submissions = r.subreddit('pipetobaccomarket').new(limit=20)
+    # iterate through them
+    for x in submissions:
+        # Discard all Wanting to Trade/Wanting to Buy posts, leaving only Wanting to Sell or untagged posts
+        match = re.search('WT[TB]', str(x.title))
+        # Pull the list of ids from the database
+        already_done = getExisting()
+        if x.id not in already_done and not match:
+            # strip title of post and assemble message
+            stripped_title = str(re.sub(r'([0-9]+\s::\s)', '', str(x.title), flags=re.IGNORECASE))
+            short_link = 'http://redd.it/{}'.format(x.id)
+            message = stripped_title + ' - ' + short_link
+            # print out to stdout
+            print('Message sent:  New r/pipetobaccomarket Post - {}'.format(message))
+            # send message via Pushover
+            client.send_message(message, title="New r/pipetobaccomarket Post", priority=1)
+            # Log to syslog for documentation sake (will be replaced by logger at some point)
+            syslog.syslog('PushReddit - Message sent:  New r/pipetobaccomarket Post - ' + message)
+            # Add post ID to the database
+            insert(x.id, stripped_title, short_link, x.created_utc)
 
-# Run main
+def main_loop():
+    while 1:
+        main()
+        time.sleep(600)
+
+# Run main_loop
 if __name__ == '__main__':
-	try:
-		main()
-		s.enter(300, 1, main, ())
-		s.run()
-	except:
-		db.close()
+    try:
+        main_loop()
+    except KeyboardInterrupt:
+        db.close()
+        sys.stderr.write('\nExiting by user request.\n')
+        sys.exit(0)
+    except:
+        db.close()
+        sys.stderr.write('\nExiting due to unknown cause.\n')
+        print(sys.exc_info()[0])
+        sys.exit(0)
